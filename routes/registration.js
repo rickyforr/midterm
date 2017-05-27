3ed2abd5b17aabc10e546efe0654a1a513f75a31
Richard Forrester <richard.forrester@gmail.com>
2017-05-27 21:58:22 +0000

    added registration.js to routes folder

---
+++ routes/registration.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const db = require('../db/queries.js');
+
+const express = require('express');
+const router  = express.Router();
+//const bcrypt  = require('bcrypt');
+
+
+
+module.exports = function (knex) {
+
+  //This is the Registration//
+
+  router.post("/", (req, res) => {
+
+    const email = req.body.email;
+    const username = req.body.uname
+    const password = req.body.pword
+
+    let user = {}
+      user.name = username
+      user.email = email
+      user.password = password
+
+    db(knex).saveUser(user, function(user) {
+
+
+
+  //          res.status(200).redirect("/api/users");
+   db(knex).getResourcesByUser(user.id, function(resourcesFromDB) {
+
+            res.status(200).render("resource");
+          });
+
+
+    })
+
+  })
+
+  return router;
+
+}
+
+
+
+
